d869015b7ba445477a26f3263c31ad7798237d07
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-01 01:45:54 -0700

    established routes, html, and javascript files

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendsArray = [
+    {
+      customerName: "Ahmed",
+      customerEmail: "ahmed@example.com",
+      customerID: "afhaque89",
+      phoneNumber: "000-000-0000"
+    }
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -14,3 +14,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
