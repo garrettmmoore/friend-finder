76d007fed44177430e8fcf2c04a698f1d8f674ca
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-01 10:22:21 -0700

    remove comments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,17 +2,14 @@ var path = require("path");
 
 module.exports = function(app) {
     // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
-  
+
     app.get("/survey", function(req, res) {
       res.sendFile(path.join(__dirname, "/../public/survey.html"));
       res.sendFile(path.join(__dirname, "/css/style.css"));
     });
   
     // app.get("/home", function(req, res) {
-    //   res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    //   res.sendFile(path.join(__dirname, "/../public/home.html"));
     // });
   
     // If no matching route is found default to home
